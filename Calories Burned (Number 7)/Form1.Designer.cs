cc6cfc5a33ecb8eb698ddf07926b4df822a948ea
dhaas01 <77082867+dhaas01@users.noreply.github.com>
2021-05-12 22:00:31 -0400

    Add files via upload

---
+++ Calories Burned (Number 7)/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Calories_Burned__Number_7_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.caloriesBurnedListBox = new System.Windows.Forms.ListBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // caloriesBurnedListBox
+            // 
+            this.caloriesBurnedListBox.FormattingEnabled = true;
+            this.caloriesBurnedListBox.Location = new System.Drawing.Point(12, 20);
+            this.caloriesBurnedListBox.Name = "caloriesBurnedListBox";
+            this.caloriesBurnedListBox.Size = new System.Drawing.Size(371, 199);
+            this.caloriesBurnedListBox.TabIndex = 0;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(82, 242);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(227, 242);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 298);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.caloriesBurnedListBox);
+            this.Name = "Form1";
+            this.Text = "Calories Burned";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox caloriesBurnedListBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Calories Burned (Number 7)/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calories_Burned__Number_7_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            const double CALORIES_PER_MINUTE = 3.9;         // The number of calories burned per minute.
+            int counterMinutes = 10;                        // The number of minutes; needs to start at 10.
+            double caloriesBurnedAfterXMinutes = 0;         // Holds the calculated number of calories burned.
+            const int MAXIMUM_NUMBER_MINUTES = 30;          // The maximum number of minutes that need to be calculated for.
+            // While loop executes while the number of minutes is less than 30.
+            while (counterMinutes < MAXIMUM_NUMBER_MINUTES)
+            { 
+                // Calculates the calories burned.
+                caloriesBurnedAfterXMinutes = counterMinutes * CALORIES_PER_MINUTE; 
+                // Displays the calories burned in the ListBox.
+                caloriesBurnedListBox.Items.Add("After " + counterMinutes + " minutes, " +
+                    caloriesBurnedAfterXMinutes + " calories are burned."); 
+                // Will increment the counter minutes by 5 because the number of minutes needs to be 10, 15, 20, etc.
+                counterMinutes = counterMinutes + 5;
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
